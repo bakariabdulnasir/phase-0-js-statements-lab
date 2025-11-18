e94a73f127c94c23735419884bcf843c36e929ee
bakari <anbphones@icloud.com>
2025-11-18 10:59:53 +0300

    My Javascript Statements

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,28 @@
 const temperature = 90;
 const timeOfDay = "morning";
-let soilMoisture = 30;
+let soilMoisture= 30;
+
+
+if(temperature>80){
+    console.log("Watering on")
+}else{
+    console.log("Watering off")
+}
+
+//if else statement 1
+
+if (timeOfDay =='Morning '|| timeOfDay=='Night'){
+    console.log("lights on")
+}else{
+    console.log("Lights off")
+}
+
+//if else statement 2
+ 
+while (soilMoisture < 40) {
+  console.log(soilMoisture);
+  soilMoisture += 5;  // increment by 5
+}
+
+// When it reaches 40, log 40
+console.log(soilMoisture);
